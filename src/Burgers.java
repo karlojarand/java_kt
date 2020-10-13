09b6d8e608fee8d21298bd22b637e555f9cbc6d5
Karl Ojarand <karl@play.ee>
2020-10-13 17:10:08 +0300

    Ülesanne 1

---
+++ src/Burgers.java
@@ -0,0 +1,10 @@
+/*
+public class Burgers {
+    public static void main(String[] args) {
+        String burger;
+        String lihaTyyp;
+        String saiaTyyp;
+        double hind =;
+    }
+}
+*/
