e3ee5e98deda7a101009d8a701dc5c8bc0df9330
Karl Ojarand <karl@play.ee>
2020-10-13 17:09:35 +0300

    Ülesanne 1 numbrid

---
+++ src/Numbers.java
@@ -0,0 +1,14 @@
+public class Numbers {
+    public static void main(String[] args) {
+
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 9 ; j >= 0; j--) {
+                    int rowMaxNumber = 10 - i;
+                    System.out.print(Math.min(rowMaxNumber, j));
+                }
+                System.out.print('\n');
+            }
+    }
+}
+
+
